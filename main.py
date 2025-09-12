e8fa29432bcc4473472c96ca02ad2f952eb197d3
elyff <jeaneela02@gmail.com>
2025-09-12 14:30:16 +0000

    Aula 12/09 - Listas

--- main.py
+++ main.py
@@ -2,4 +2,59 @@
 # Revisão de Orientação a Objetos
 # Prof. Guilherme Leal Santos
 
-print('Bem vindos!')
+frutas = ['Maça' , 'Banana' , 'Laranja'] # Lista de frutas
+print(frutas)
+print(frutas[0])
+print(f'Quantidade de frutas: {len(frutas)}')
+
+frutas.append('Uva') # Adiciona um elemento ao final da lista
+print(frutas)
+
+frutas.insert(1, 'Abacaxi') # Adiciona um elemento na posição desejada
+print(frutas)
+
+#frutas = frutas.pop() # Remove o último elemento da lista
+#frutas = frutas.pop(-1) # Remove o último elemento da lista
+# frutas = frutas.pop(0) # Remove elemento da posição desejada
+# print(f'Removida: {frutas}')
+# print(frutas)
+
+frutas.remove('Uva') # Remove o elemento desejado
+print(frutas)
+
+numeros = [5, 3, 8, 1, 4]
+print(numeros)
+
+numeros_ord_c = sorted(numeros) # Ordena a lista em ordem crescente
+print(f'Ordenada (c) : {numeros_ord_c}')
+
+numeros_ord_d = sorted(numeros, reverse=True) # Ordena a lista em ordem decrescente
+print(f'Ordenada (d) : {numeros_ord_d}')
+
+numeros_dobrados = []
+for n in numeros:
+    numeros_dobrados.append(n * 2) # Dobra o valor de cada elemento da lista
+print(f'Números dobrados: {numeros_dobrados}')
+numeros_dobrados = list(map(lambda n: n * 2, numeros)) # Dobra o valor de cada elemento da lista usando map e lambda
+print(f'Números dobrados (map): {numeros_dobrados}')
+
+#numeros_filtrados = []
+#for n in numeros:
+#    if n > 4:
+#        numeros_filtrados.append(n) # Filtra os números maiores que 4
+#print(f'Números filtrados: {numeros_filtrados}')
+numeros_filtrados = list(filter(lambda n: n > 4, numeros)) # Filtra os números maiores que 4 usando filter e lambda
+print(f'Números filtrados (filter): {numeros_filtrados}')
+
+soma = 0 
+for n in numeros:
+    soma += n # Soma todos os elementos da lista
+print(soma)
+
+from functools import reduce
+soma = reduce (lambda soma, n: soma + n, numeros) # Soma todos os elementos da lista usando reduce e lambda
+print(soma)
+    
+
+
+
